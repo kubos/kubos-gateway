cadfef11816b2207499a443105c70c2d255ad994
Andrew Cantino <cantino@users.noreply.github.com>
2018-09-12 14:34:09 -0700

    Allow selection of config file as first command argument

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 import json
 import logging
+import sys
 
 from kubos_adapter.adapter import Adapter
 
-with open('config/config.local.json', 'r') as configfile:
+if len(sys.argv) > 1:
+  config_file = sys.argv[1]
+else:
+  config_file = 'config/config.local.json'
+
+with open(config_file, 'r') as configfile:
     config = json.loads(configfile.read())
 
 Adapter.set_log_level(logging.DEBUG, very_verbose=False)
