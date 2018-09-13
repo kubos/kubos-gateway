13e8e8453eb848dbf393f4a73d0f500d5e12e884
Andrew Cantino <cantino@users.noreply.github.com>
2018-09-13 13:32:48 -0700

    Allow comments in JSON for convenience

--- run.py
+++ run.py
@@ -5,12 +5,13 @@ import sys
 from kubos_adapter.adapter import Adapter
 
 if len(sys.argv) > 1:
-  config_file = sys.argv[1]
+    config_file = sys.argv[1]
 else:
-  config_file = 'config/config.local.json'
+    config_file = 'config/config.local.json'
 
 with open(config_file, 'r') as configfile:
-    config = json.loads(configfile.read())
+    # Allow "comments" in JSON for convenience.
+    config = json.loads("\n".join([line for line in configfile if not line.startswith('/') or line.startswith('#')]))
 
 Adapter.set_log_level(logging.DEBUG, very_verbose=False)
 Adapter.run_forever(config)
